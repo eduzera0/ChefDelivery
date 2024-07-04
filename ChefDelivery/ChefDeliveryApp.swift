61a1683dd90551ac4f355410fc777e60575ab2a1
Eduardo Moretto <edumoretto21051@gmail.com>
2024-07-03 22:28:15 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Eduardo Moretto on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
